3e989dfd814c5814e6d0ac6f94e53f5bd1b35858
DoSafills <ricardo.erest@gmail.com>
2025-06-29 14:17:29 -0400

    Delete proyecto2 directory

--- proyecto2/sim/init_simulator.py
+++
@@ -1,6 +0,0 @@
-# sim/init_simulator.py
-from sim.simulator import OrderSimulator
-
-def init_simulator():
-    if 'simulador' not in st.session_state:
-        st.session_state.simulador = OrderSimulator()
